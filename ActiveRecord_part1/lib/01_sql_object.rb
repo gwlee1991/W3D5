b0aa742cf8527c5e784fb9c4f536ea5264ea810d
%= GitConfig.user_name % <%= GitConfig.user_email %>
2017-06-16 12:35:15 -0700

    initialize SQL object

--- ActiveRecord_part1/lib/01_sql_object.rb
+++ ActiveRecord_part1/lib/01_sql_object.rb
@@ -5,18 +5,34 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    @columns ||= DBConnection.execute2(<<-SQL)
+      SELECT
+      *
+      FROM
+      "#{table_name}"
+    SQL
+      .first.map{|column| column.to_sym}
   end
 
   def self.finalize!
+
+    columns.each do |column|
+      define_method (column) do
+        attributes[column]
+      end
+
+      define_method ("#{column}=") do |value|
+        attributes[column] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    "#{self} = #{table_name}"
   end
 
   def self.table_name
-    # ...
+    "#{self}s".downcase
   end
 
   def self.all
@@ -32,11 +48,18 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    columns = self.class.columns
+    params.each do |key, value|
+      if columns.include?(key)
+        self.send "#{key}=", value
+      else
+        raise "unknown attribute '#{key}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
