cfd11c179687302bf717b01273410af9ff2bbde2
%= GitConfig.user_name % <%= GitConfig.user_email %>
2017-06-16 22:08:56 -0700

    finish phase1 of activerecord lite

--- ActiveRecord_part1/lib/01_sql_object.rb
+++ ActiveRecord_part1/lib/01_sql_object.rb
@@ -36,21 +36,36 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    result = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM "#{self.table_name}"
+    SQL
+    parse_all(result)
+
   end
 
   def self.parse_all(results)
-    # ...
+    parsed = []
+
+    results.each do |hash|
+      parsed << self.new(hash)
+    end
+    parsed
   end
 
   def self.find(id)
-    # ...
+    results= DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM "#{self.table_name}"
+      WHERE id = "#{id}"
+    SQL
+      parse_all(results).first
   end
 
   def initialize(params = {})
     columns = self.class.columns
     params.each do |key, value|
-      if columns.include?(key)
+      if columns.include?(key.to_sym)
         self.send "#{key}=", value
       else
         raise "unknown attribute '#{key}'"
@@ -63,18 +78,49 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    attributes.values
   end
 
   def insert
-    # ...
+    columns = self.class.columns.drop(1)
+    id = self.class.columns.first
+    col_names = columns.join(', ')
+    question_marks = []
+    columns.length.times do
+      question_marks << "?"
+    end
+    joint = question_marks.join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+      (#{joint})
+    SQL
+
+    attributes[id] = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    columns = self.class.columns.map{|column| "#{column} = ?"}.drop(1)
+    row = columns.join(', ')
+    id = self.class.columns.first
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{row}
+      WHERE
+        id = #{attributes[:id]}
+    SQL
   end
 
   def save
-    # ...
+    if attributes[:id].nil?
+      insert
+    else
+      update
+    end
   end
 end
--- ActiveRecord_part1/lib/02_searchable.rb
+++ ActiveRecord_part1/lib/02_searchable.rb
@@ -3,10 +3,15 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    values = params.values
+    where_line = params.keys.map{|key|"#{key} = ?"}
+    joint = where_line.join(' AND ')
+
+    p joint
+    p values
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  include Searchable
 end
